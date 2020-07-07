ad6a4ed8b836e5f070e909ebd84c548f938c57c3
Andrej <andrejilicb@gmail.com>
2020-07-07 15:34:14 +0200

    Fix global BTTV emotes
    Remove channel specific BTTV emotes because the BTTV API no longer supports it

--- twitch.js
+++ twitch.js
@@ -3,36 +3,23 @@ const channel = params.get("channel");
 const emotes = {};
 
 const fetchEmotes = () => {
-  fetch("https://api.betterttv.net/2/emotes")
-    .then(res => res.json())
-    .then(res => {
-      res.emotes.forEach(emote => {
-        emotes[emote.code] = res.urlTemplate
-          .replace("{{id}}", emote.id)
-          .replace("{{image}}", "3x");
+  fetch("https://api.betterttv.net/3/cached/emotes/global")
+    .then((res) => res.json())
+    .then((res) => {
+      res.forEach((emote) => {
+        emotes[emote.code] = `https://cdn.betterttv.net/emote/${emote.id}/3x`;
       });
     })
-    .catch(err => console.error(err));
-
-  fetch(`https://api.betterttv.net/2/channels/${channel}`)
-    .then(res => res.json())
-    .then(res => {
-      res.emotes.forEach(emote => {
-        emotes[emote.code] = res.urlTemplate
-          .replace("{{id}}", emote.id)
-          .replace("{{image}}", "3x");
-      });
-    })
-    .catch(err => console.error(err));
+    .catch((err) => console.error(err));
 
   fetch(`https://api.frankerfacez.com/v1/room/${channel}`)
-    .then(res => res.json())
-    .then(res => {
-      res.sets[res.room.set].emoticons.forEach(emote => {
+    .then((res) => res.json())
+    .then((res) => {
+      res.sets[res.room.set].emoticons.forEach((emote) => {
         emotes[emote.name] = emote.urls[Object.keys(emote.urls).pop()];
       });
     })
-    .catch(err => console.error(err));
+    .catch((err) => console.error(err));
 };
 
 const init = () => {
@@ -40,12 +27,12 @@ const init = () => {
 
   ComfyJS.onChat = (user, message, flags, self, extra) => {
     if (extra.messageEmotes) {
-      Object.keys(extra.messageEmotes).forEach(emoteId => {
+      Object.keys(extra.messageEmotes).forEach((emoteId) => {
         showEmote(`https://static-cdn.jtvnw.net/emoticons/v1/${emoteId}/3.0`);
       });
     }
 
-    message.split(" ").forEach(word => {
+    message.split(" ").forEach((word) => {
       if (emotes[word]) {
         showEmote(emotes[word]);
       }
